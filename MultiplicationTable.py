544699b240f782b9acecdde711394902807e6d75
balajigudivada <94883673+balajigudivada@users.noreply.github.com>
2022-01-04 14:14:06 +0530

    Update MultiplicationTable.py

--- MultiplicationTable.py
+++ MultiplicationTable.py
@@ -1,15 +1,10 @@
-n=int(input('Enter n value '))
-i=1
-while (i<=10):
-    v=i*n
-    print(n,'*',i,'=',v,sep=' ')
-    i=i+1
-a=int(input('Do you want to continue printing the table, press 1 or 0 to Exit:'))
+n=int(input('Enter number '))
+a=1
 while(a!=0):
-    n=n+1
-    for i in range(1,11):
-        b=n*i
-        print(n,'*',i,'=',b,sep=' ')
+    i=1
+    while(i<=10):
+        print(n,'*',i,'=',n*i)
+        i+=1
     a=int(input('Do you want to continue printing the table, press 1 or 0 to Exit:'))
-        
+    n=n+1   
     
